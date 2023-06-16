fef42562a855b1e65b6d9715b7835c11df9f1402
abelmorad <abelmorad97@gmail.com>
2023-06-16 21:43:50 +0800

    style: Used Prettier for readable code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import './scss/main.scss'
+import "./scss/main.scss";
 
-import SignIn from './components/SignIn'
-import SignUp from './components/SignUp'
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<SignIn />} />
-        <Route path='signup' element={<SignUp />}/>
+        <Route path="/" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,25 +1,45 @@
-import logo from '../../public/logo.svg'
+import logo from "../../public/logo.svg";
 
 function SignIn() {
   return (
-    <main className='sign-in-container'>
-        <header className='sign-in-header-wrapper'>
-            <div className="sign-in-logo-wrapper">
-                <img style={{height: '40px', width: '40px'}} src={logo} alt="audio tek logo"/>
-                <h1>Audio Tek</h1>
-            </div>
-            <p>It's modular and designed to last</p>
-        </header>
-        <form className='sign-in-form-wrapper'>
+    <main className="sign-in-container">
+      <header className="sign-in-header-wrapper">
+        <div className="sign-in-logo-wrapper">
+          <img
+            style={{ height: "40px", width: "40px" }}
+            src={logo}
+            alt="audio tek logo"
+          />
+          <h1>Audio Tek</h1>
+        </div>
+        <p>It's modular and designed to last</p>
+      </header>
+      <form className="sign-in-form-wrapper">
         <h2>Sign In</h2>
-            <input className='email-input' type="email" name="email" id="emailInput" placeholder='Email' />
-            <input className='password-input' type="password" name="password" id="passwordInput" placeholder='Password' />
-            <a href="#">Forgot Password?</a>
-            <button className='sign-in-btn button-primary' type="submit">Sign In</button>
-            <p>Don't have an account? <a href="#">Sign Up Here</a></p>
-        </form>
+        <input
+          className="email-input"
+          type="email"
+          name="email"
+          id="emailInput"
+          placeholder="Email"
+        />
+        <input
+          className="password-input"
+          type="password"
+          name="password"
+          id="passwordInput"
+          placeholder="Password"
+        />
+        <a href="#">Forgot Password?</a>
+        <button className="sign-in-btn button-primary" type="submit">
+          Sign In
+        </button>
+        <p>
+          Don't have an account? <a href="#">Sign Up Here</a>
+        </p>
+      </form>
     </main>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
