cf7940c83d463ddfc0c91567c75bf1ebd78ac974
abelmorad <abelmorad97@gmail.com>
2023-06-17 08:20:09 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./scss/main.scss";
 
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="signup" element={<SignUp />} />
+        <Route path="home" element={<Home />} />
       </Routes>
     </>
   );
