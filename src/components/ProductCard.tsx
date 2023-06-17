df57f800e9f3dcad3824c8f1f0530ea2dc57ce14
abelmorad <abelmorad97@gmail.com>
2023-06-17 21:16:55 +0800

    Feat: Card using SwiperJS and React ROuter

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+function ProductCard( {src, alt,category}: {category:string,src:string, alt:string} ) {
+  return (
+    <section className="category-card">
+        <div className="card-title-wrapper">
+          <h2>{category}</h2>
+          <Link className="shop-now" to='#'>Shop now</Link>
+        </div>
+        <figure>
+          <img src={src} alt={alt} />
+        </figure>
+    </section>
+  )
+}
+
+export default ProductCard
