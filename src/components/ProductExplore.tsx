4d6756ad612ee7f44d0ebacd08e2ca317d4cf424
abelmorad <abelmorad97@gmail.com>
2023-06-22 06:49:55 +0800

    Feat: now with heading and filter

--- src/components/ProductExplore.tsx
+++ src/components/ProductExplore.tsx
@@ -2,20 +2,33 @@ import Nav from "./Nav";
 import BackBtn from "./BackBtn";
 import CartBtn from "./CartBtn";
 import Logo from "./Logo";
+import Filter from "./Filter";
 
 function ProductExplore() {
   return (
-    <section className="product-explore">
-      <Nav
-        nav={
-          <>
-            <BackBtn to={"/home"} />
-            <Logo />
-            <CartBtn />
-          </>
-        }
-      />
-    </section>
+    <>
+      <section className="product-explore">
+        <Nav
+          nav={
+            <>
+              <BackBtn to={"/home"} />
+              <Logo />
+              <CartBtn />
+            </>
+          }
+        />
+        <div className="__heading">
+          <p>Headphone</p>
+          <h2>Products</h2>
+        </div>
+        <section className="__filter--container">
+          <Filter />
+        </section>
+      </section>
+      <section className="products__card">
+        
+      </section>
+    </>
   );
 }
 
