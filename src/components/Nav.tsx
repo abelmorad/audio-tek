bbb0449241828018943412c4726d1d774d8c6def
abelmorad <abelmorad97@gmail.com>
2023-06-21 15:03:22 +0800

    Feat: Nav menu now a component instead of hardcoded into home  page

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+
+function Nav() {
+  return (
+    <nav>
+        <div className="menu">
+            <div className="menu-content">
+                <p>AsdADADAD</p>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
