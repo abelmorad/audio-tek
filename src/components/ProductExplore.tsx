6407adee688760035493569d58433c44b450b574
abelmorad <abelmorad97@gmail.com>
2023-06-21 17:35:58 +0800

    Feat: explore products page

---
+++ src/components/ProductExplore.tsx
@@ -0,0 +1,9 @@
+function ProductExplore() {
+    return (
+      <section className="product-explore">
+          <p>3123123131</p>
+      </section>
+    )
+  }
+  
+  export default ProductExplore
