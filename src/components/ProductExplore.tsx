473a27925ccd62960fed1f229bfe9946861a3b00
abelmorad <abelmorad97@gmail.com>
2023-06-22 09:56:12 +0800

    TEST: allproductcards feature testing

--- src/components/ProductExplore.tsx
+++ src/components/ProductExplore.tsx
@@ -4,6 +4,13 @@ import CartBtn from "./CartBtn";
 import Logo from "./Logo";
 import Filter from "./Filter";
 
+import AllProductCard from "./AllProductCard";
+
+import maharishiwaistbag from "../../public/allproducts/maharishibag.png";
+import c17adapterneon from "../../public/allproducts/c17adapterneon.png";
+import tma2studiowireless from "../../public/allproducts/tma2studiowireless.png";
+import tracks from "../../public/allproducts/tracks.png";
+
 function ProductExplore() {
   return (
     <>
@@ -26,7 +33,42 @@ function ProductExplore() {
         </section>
       </section>
       <section className="products__card">
-        
+        <AllProductCard
+          src={maharishiwaistbag}
+          alt={maharishiwaistbag}
+          productname="Maharishi Waist Bag"
+          currency="USD"
+          price={120}
+          rating={4}
+          reviews={50}
+        />
+        <AllProductCard
+          src={tma2studiowireless}
+          alt={tma2studiowireless}
+          productname="TMA-2 Studio Wireless Headphone"
+          currency="USD"
+          price={350}
+          rating={4.9}
+          reviews={250}
+        />
+        <AllProductCard
+          src={c17adapterneon}
+          alt={c17adapterneon}
+          productname="C17 Staight - 1.2m - Remote with mic"
+          currency="USD"
+          price={25}
+          rating={4}
+          reviews={29}
+        />
+        <AllProductCard
+          src={tracks}
+          alt={tracks}
+          productname="Tracks"
+          currency="USD"
+          price={65}
+          rating={3.5}
+          reviews={49}
+        />
       </section>
     </>
   );
