ce976f3b3598ac351b93a2d47399a7161040949c
abelmorad <abelmorad97@gmail.com>
2023-06-22 09:55:18 +0800

    Refactor:

--- src/components/FeaturedCard.tsx
+++ src/components/FeaturedCard.tsx
@@ -1,27 +1,29 @@
 function FeaturedCard({
-    src,
-    alt,
-    productname,
-    currency,
-    price,
-  }: {
-    src: string;
-    alt: string;
-    productname: string;
-    currency: string;
-    price: number;
-  }) {
+  src,
+  alt,
+  productname,
+  currency,
+  price,
+}: {
+  src: string;
+  alt: string;
+  productname: string;
+  currency: string;
+  price: number;
+}) {
   return (
     <section className="featured-card-wrapper">
-        <figure>
-            <img className="featured-card-img" src={src} alt={alt} />
-        </figure>
-        <div className="item-price-wrapper">
-            <p>{productname}</p>
-            <b>{currency} {price}</b>
-        </div>
+      <figure>
+        <img className="featured-card-img" src={src} alt={alt} />
+      </figure>
+      <div className="item-price-wrapper">
+        <p>{productname}</p>
+        <b>
+          {currency} {price}
+        </b>
+      </div>
     </section>
-  )
+  );
 }
 
-export default FeaturedCard
+export default FeaturedCard;
