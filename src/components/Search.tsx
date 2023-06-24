31058bd52f6fdc4e3e46fc793a8ccbffb11d3bd8
abelmorad <abelmorad97@gmail.com>
2023-06-24 09:34:54 +0800

    Style: update style

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,6 +3,9 @@ import BackBtn from "./BackBtn";
 import CartBtn from "./CartBtn";
 import SearchBar from "./SearchBar";
 import CloseBtn from "./CloseBtn";
+import PopularProductCard from "./PopularProductCard";
+
+import tma2 from "../../public/allproducts/tma2studiowireless.png"
 
 function Search() {
   return (
@@ -21,15 +24,53 @@ function Search() {
         <p>Latest search</p>
         <div className="history--wrapper">
           <div className="history">
-            <span>TMA2 Wireles1313s</span> <CloseBtn />
+            <span>TMA2 Wireles1313s</span>
+            <CloseBtn />
           </div>
           <div className="history">
-            <span>TMA2 Wireles1313s</span> <CloseBtn />
+            <span>TMA2 Wireles1313s</span>
+            <CloseBtn />
           </div>
         </div>
       </section>
       <section className="popular-product">
         <p>Popular product</p>
+        <PopularProductCard
+        popularCardSrc={tma2}
+        popularCardAlt={tma2}
+        popularProductName="TMA-2 Studio Wireless"
+        currency="USD"
+        price={350}
+        rating={4.9}
+        reviews={250}
+        />
+        <PopularProductCard
+        popularCardSrc={tma2}
+        popularCardAlt={tma2}
+        popularProductName="TMA-2 Studio Wireless"
+        currency="USD"
+        price={350}
+        rating={4.9}
+        reviews={250}
+        />
+        <PopularProductCard
+        popularCardSrc={tma2}
+        popularCardAlt={tma2}
+        popularProductName="TMA-2 Studio Wireless"
+        currency="USD"
+        price={350}
+        rating={4.9}
+        reviews={250}
+        />
+        <PopularProductCard
+        popularCardSrc={tma2}
+        popularCardAlt={tma2}
+        popularProductName="TMA-2 Studio Wireless"
+        currency="USD"
+        price={350}
+        rating={4.9}
+        reviews={250}
+        />
       </section>
     </section>
   );
