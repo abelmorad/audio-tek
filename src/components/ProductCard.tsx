4ac7ee052e78b3a8621dda240fb90f04e184ec44
abelmorad <abelmorad97@gmail.com>
2023-06-26 06:58:23 +0800

    Feat: data now from api

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,26 +1,43 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import "swiper/swiper-bundle.esm.js";
+import "swiper/css/autoplay";
 
-function ProductCard({
-  src,
-  alt,
-  category,
-}: {
-  category: string;
-  src: string;
-  alt: string;
-}) {
+function ProductCard() {
+  const [productData, setProductData] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("/src/productdata.json")
+      .then((res) => {
+        setProductData(res.data.products.slice(0,4));
+        console.log(res.data.products);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <Link to={"/product"}>
-      <section className="category-card">
-        <div className="card-title-wrapper">
-          <h2>{category}</h2>
-          <p>Shop now</p>
-        </div>
-        <figure>
-          <img src={src} alt={alt} />
-        </figure>
-      </section>
-    </Link>
+    <Swiper autoplay pagination spaceBetween={10} slidesPerView={1}>
+      {productData.map((data) => (
+          <SwiperSlide key={data.key}>
+            <Link to={"/product"} >
+            <section className="category-card">
+              <div className="card-title-wrapper">
+                <h2>{data.productName}</h2>
+              </div>
+              <figure>
+                <img src={data.image} alt={data.productName} />
+                <p>Shop now</p>
+              </figure>
+            </section>
+          </Link>
+          </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
 
