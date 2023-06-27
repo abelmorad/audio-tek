d3f587eb81646ceff1c8ab61134158ea4f2c0b6b
abelmorad <abelmorad97@gmail.com>
2023-06-27 14:02:22 +0800

    TEST: search feature

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,74 +1,55 @@
 import Nav from "./Nav";
 import BackBtn from "./BackBtn";
+import CloseBtn from "./CloseBtn";
 import CartBtn from "./CartBtn";
 import SearchBar from "./SearchBar";
 import PopularProductCard from "./PopularProductCard";
 
-import tma2 from "../../public/allproducts/tma2studiowireless.png"
+import { useState, useRef } from "react";
 
 function Search() {
+  const [searchItems, setSearchItems] = useState<any>([]);
+  const inputRef = useRef<any>();
+
+  function onSubmit(e: any) {
+    e.preventDefault();
+
+    const value = inputRef.current.value;
+    if (value === "") return;
+    setSearchItems((prev: any) => {
+      return [...prev, value];
+    });
+    inputRef.current.value = "";
+  }
 
   return (
     <section className="search">
       <Nav
         nav={
           <>
-            <BackBtn to="/product"/>
+            <BackBtn to="/product" />
             <p>Search</p>
             <CartBtn />
           </>
         }
       />
-      <SearchBar />
+      <form onSubmit={onSubmit}>
+        <SearchBar referrence={inputRef} />
+      </form>
       <section className="search__latest">
         <p>Latest search</p>
         <div className="history--wrapper">
-          <div className="history">
-            <span>TMA2 Wireles1313s</span>
-          </div>
-          <div className="history">
-            <span>TMA2 Wireles1313s</span>
-          </div>
+          {searchItems.map((item: any) => (
+            <div className="history">
+              <span>{item}</span>
+              <CloseBtn />
+            </div>
+          ))}
         </div>
       </section>
       <section className="popular-product">
         <p>Popular product</p>
-        <PopularProductCard
-        popularCardSrc={tma2}
-        popularCardAlt={tma2}
-        popularProductName="TMA-2 Studio Wireless"
-        currency="USD"
-        price={350}
-        rating={4.9}
-        reviews={250}
-        />
-        <PopularProductCard
-        popularCardSrc={tma2}
-        popularCardAlt={tma2}
-        popularProductName="TMA-2 Studio Wireless"
-        currency="USD"
-        price={350}
-        rating={4.9}
-        reviews={250}
-        />
-        <PopularProductCard
-        popularCardSrc={tma2}
-        popularCardAlt={tma2}
-        popularProductName="TMA-2 Studio Wireless"
-        currency="USD"
-        price={350}
-        rating={4.9}
-        reviews={250}
-        />
-        <PopularProductCard
-        popularCardSrc={tma2}
-        popularCardAlt={tma2}
-        popularProductName="TMA-2 Studio Wireless"
-        currency="USD"
-        price={350}
-        rating={4.9}
-        reviews={250}
-        />
+        <PopularProductCard />
       </section>
     </section>
   );
